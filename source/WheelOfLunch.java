e6d8ccfed631fe6fc597cb8f68b75144c6149d14
spthiessen <spthiessen@gmail.com>
2017-04-13 13:14:20 -0600

    Remote List
    Will now remotely get the list of restaurants from a web site.

--- source/WheelOfLunch.java
+++ source/WheelOfLunch.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.net.*;
 
 public class WheelOfLunch {
 	
@@ -9,66 +10,35 @@ public class WheelOfLunch {
         JOptionPane.showMessageDialog(null, infoMessage, "InfoBox: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
     }
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws FileNotFoundException, MalformedURLException, IOException {
 		// TODO Auto-generated method stub
 		
 		//The list of restaurants
 		ArrayList<String> restaurant = new ArrayList<String>();
 
 		//Load the external file containing the list of restaurants
-		Scanner s = new Scanner(new File("restfile.txt"));
+		URL url = new URL ("http://www.nekec.com/wheel-of-lunch/restfile.txt");
 		
-		//Add the restaurants from the external file
-		while (s.hasNext() ) {
-			restaurant.add(s.nextLine() );
-			
+		URLConnection conn = url.openConnection();
+
+		BufferedReader br = new BufferedReader (
+				new InputStreamReader (conn.getInputStream() ) );
+		
+		String inputLine;
+		
+		while ((inputLine = br.readLine()) != null) {
+			restaurant.add(inputLine);
 		}
 		
-		s.close();
 		
-		/*
-		//The List of restaurants
-		restaurant.add("Montana's");
-		restaurant.add("Pizza Hut");
-		restaurant.add("Venice House on 2nd");
-		restaurant.add("Boston's Pizza");
-		restaurant.add("McDonald's");
-		restaurant.add("Wendy's");
-		restaurant.add("Mucho Burrito");
-		restaurant.add("Fatburger");
-		restaurant.add( "Burger King");
-		restaurant.add("Arby's");
-		restaurant.add("Mr. Mike's");
-		restaurant.add("Aisian Le");
-		restaurant.add("Grainfields");
-		restaurant.add("Aisa Buffett");
-		restaurant.add("Panago");
-		restaurant.add("Rusty Owl");
-		restaurant.add("Venice Houce on Central");
-		restaurant.add("Chicken Chef");
-		restaurant.add("Spicy Peppercorn");
-		restaurant.add("Vu's Garden");
-		restaurant.add("Spice Trail");
-		restaurant.add("A&W");
-		restaurant.add("Dairy Queen");
-		restaurant.add("Taco Time");
-		restaurant.add("Extreme Pita");
-		restaurant.add("Subway");
-		restaurant.add("Quizno's");
-		restaurant.add("Mr. Sub");
-		restaurant.add("Selena's Donair");
-		*/
-
 		int listLength = restaurant.size();
-		
-		//System.out.println("There are " + listLength + " restaurants in our list.");
 		
 		// Pick the restaurant to eat at
 		Random random = new Random();
 		int lunch = random.nextInt(listLength);
 		
-		//System.out.println("Let's eat at "+ restaurant.get(lunch) +"." );
-		
+		//Display all the information
+		WheelOfLunch.infoBox("Generating list of restaurants from " + url + ".", "Where is the List?");
 		WheelOfLunch.infoBox("There are " + listLength + " restaurants in our list.", "How Many Restarants?");
 		WheelOfLunch.infoBox("Let's eat at "+ restaurant.get(lunch) +".", "Where To Eat!");
 		
